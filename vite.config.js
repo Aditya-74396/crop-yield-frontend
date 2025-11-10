a36c38c16ce41d3f66f987ad70b0e8fef13eae84
Aditya <athakur0412@gmail.com>
2025-11-11 00:26:31 +0530

    Connected frontend to deployed backend

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/crop-yield-frontend/'
+  base: '/',
+  server: {
+    port: 5173,
+  },
 })
 
